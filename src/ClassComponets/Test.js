87ad03fba9a1bdf54956c987d88d2ff74cc0d4f8
shaileshbalar007 <amishapatel2602@gamil.com>
2024-05-15 14:58:52 +0530

    six  commit

--- src/ClassComponets/Test.js
+++ src/ClassComponets/Test.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-class Test extends React.Component(){
-    render(){
-        return(<>
-            <h1>WElcome to classs componets</h1>
-        
-            </>)
+class Test extends React.Component {
+    constructor() {
+        super()
     }
 
+    render() {
+        return (
+            <>
+                <h1>Welcome to class Component</h1>
+            </>
+        )
+    }
+
+
 }
-export default Test ;
+export default Test;
